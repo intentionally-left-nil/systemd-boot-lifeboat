899ea8b4307cc6d9c72d0087d7f7b88f4a3180fa
Anil Kulkarni <anil@terminal.space>
2022-08-12 13:37:15 -0700

    Create a new boot entry based off the existing entry, cloning the efi file

--- systemd_boot_lifeboat.py
+++ systemd_boot_lifeboat.py
@@ -1,36 +1,81 @@
 #!/usr/bin/env python
+from __future__ import annotations
+import errno
 
 import os
 import typing
 from collections import OrderedDict
+import shutil
 
 
 class Config(OrderedDict[str, str]):
-    filename: str
+    filepath: str
 
-    def __init__(self, filename: str):
-        super().__init__()
-        self.filename = filename
-        with open(self.filename, 'r', encoding='utf8') as fp:
+    def __init__(self, filepath: str, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.filepath = filepath
+
+    def load(self):
+        with open(self.filepath, 'r', encoding='utf8') as fp:
             lines = fp.readlines()
             lines = [x.strip() for x in lines]
             lines = [x for x in lines if not x.startswith('#')]
-            keyvals = [x.split() for x in lines]
+            keyvals = [x.split(maxsplit=1) for x in lines]
             keyvals = [x for x in keyvals if len(x) == 2]
             for [key, val] in keyvals:
                 self[key] = val
 
     def write(self):
-        with open(self.filename, 'w', encoding='utf8') as fp:
+        with open(self.filepath, 'x', encoding='utf8') as fp:
             lines = [f"{key}\t{value}\n" for [key, value] in self.items()]
             fp.writelines(lines)
+
+    def create_lifeboat(self, ts: int) -> Config:
+        lifeboat_name = lifeboat_path(self.filepath, ts)
+        lifeboat = Config(lifeboat_name, self)
+        try:
+            if 'efi' in self:
+                lifeboat['efi'] = lifeboat_path(self['efi'], ts)
+                copy(self['efi'], lifeboat['efi'])
+            lifeboat.write()
+        except:
+            if lifeboat['efi'] != self['efi']:
+                delete_file(lifeboat['efi'])
+            delete_file(lifeboat_name)
+            raise
+
+        return lifeboat
+
+
+def copy(src: str, target: str):
+    if os.path.exists(target):
+        raise OSError(errno.EEXIST, os.strerror(errno.EEXIST))
+    shutil.copy2(src, target)
+    st = os.stat(src)
+    os.chown(target, st.st_uid, st.st_gid)
+
+
+def delete_file(filepath: str):
+    try:
+        os.remove(filepath)
+    except OSError:
+        pass
+
+
+def lifeboat_path(filepath: str, ts: int) -> str:
+    dir = os.path.dirname(filepath)
+    name = os.path.basename(filepath)
+    return os.path.join(dir, f"lifeboat_{ts}_{name}")
 
 
 def get_default_config(esp: str) -> typing.Optional[Config]:
     try:
         loader = Config(os.path.join(esp, 'loader', 'loader.conf'))
+        loader.load()
         if 'default' in loader:
             config_filename = os.path.join(esp, 'loader', 'entries', f"{loader['default']}.conf")
-            return Config(config_filename)
+            c = Config(config_filename)
+            c.load()
+            return c
     except FileNotFoundError:
         return None
--- test_loader.py
+++ test_loader.py
@@ -1,56 +1,98 @@
 import tempfile
 import os
-from collections import OrderedDict
+import time
 import unittest
 
 from systemd_boot_lifeboat import Config, get_default_config
 
 
 class TestConfig(unittest.TestCase):
+
+    def setUp(self):
+        self.esp = tempfile.TemporaryDirectory()
+        os.makedirs(os.path.join(self.esp.name, 'loader', 'entries'), exist_ok=True)
+        super().setUp()
+
+    def tearDown(self) -> None:
+        self.esp.cleanup()
+        super().tearDown()
+
     def test_parse_config(self):
         tests = [('', {}),
                  ('onecolbad', {}),
-                 ('three cols bad', {}),
+                 ('three cols ok', {'three': 'cols ok'}),
                  ('mykey myval', {'mykey': 'myval'}),
                  ('k v\n#commentkey v2', {'k': 'v'}),
                  ('k v\n# commentkey v2', {'k': 'v'}),
                  ('k v\nk\tv2\n k3    v3', {'k': 'v2', 'k3': 'v3'}),
                  ]
-        with tempfile.TemporaryDirectory() as tmpdirname:
-            for i, test in enumerate(tests):
-                with self.subTest(test[0]):
-                    conf_name = os.path.join(tmpdirname, f'test_parse_config_{i}.conf')
-                    with open(conf_name, 'w', encoding='utf8') as fp:
-                        fp.write(test[0])
-
-                    c = Config(conf_name)
-                    self.assertDictEqual(test[1], c)
+        for i, test in enumerate(tests):
+            with self.subTest(test[0]):
+                conf_path = self.create_entry(f'test_parse_config_{i}.conf', test[0])
+                c = Config(conf_path)
+                c.load()
+                self.assertDictEqual(test[1], c)
 
     def test_write_config(self):
-        with tempfile.TemporaryDirectory() as tmpdirname:
-            conf_name = os.path.join(tmpdirname, 'test_write_config.conf')
-            with open(conf_name, 'w', encoding='utf8') as fp:
-                fp.write('k   v\n#comment\n\nk2\tv2')
-            c = Config(conf_name)
-            c.write()
-            with open(conf_name, 'r', encoding='utf8') as fp:
-                self.assertEqual('k\tv\nk2\tv2\n', fp.read())
+        conf_name = os.path.join(self.esp.name, 'test_write_config.conf')
+        c = Config(conf_name, {'k': 'v', 'k2': 'v2'})
+        c.write()
+        with open(conf_name, 'r', encoding='utf8') as fp:
+            self.assertEqual('k\tv\nk2\tv2\n', fp.read())
 
     def test_get_default_config(self):
-        with tempfile.TemporaryDirectory() as tmpdirname:
-            os.makedirs(os.path.join(tmpdirname, 'loader', 'entries'), exist_ok=True)
-            loader_name = os.path.join(tmpdirname, 'loader', 'loader.conf')
-            conf_name = os.path.join(tmpdirname, 'loader', 'entries', 'arch.conf')
-            with open(conf_name, 'w', encoding='utf8') as fp:
-                fp.write('k v')
+        self.create_entry('arch.conf', 'k v')
+        self.create_loader('missingdefault notarch')
+        self.assertEqual(None, get_default_config(self.esp.name))
 
-            with open(loader_name, 'w', encoding='utf8') as fp:
-                fp.write('missingdefault notarch')
-            self.assertEqual(None, get_default_config(tmpdirname))
+        self.create_loader('default arch')
+        self.assertEqual({'k': 'v'}, get_default_config(self.esp.name))
 
-            with open(loader_name, 'w', encoding='utf8') as fp:
-                fp.write('default arch')
-            self.assertEqual(OrderedDict({'k': 'v'}), get_default_config(tmpdirname))
+    def test_create_lifeboat(self):
+        efi_path = os.path.join(self.esp.name, 'EFI', 'Arch', 'linux.efi')
+        os.makedirs(os.path.dirname(efi_path), exist_ok=True)
+        with open(efi_path, 'w', encoding='utf8') as fp:
+            fp.write("my cool efi")
+        entry_path = self.create_entry('arch.conf', f'title my cool arch\nefi {efi_path}')
+
+        now = int(time.time())
+        c = Config(entry_path)
+        c.load()
+
+        lifeboat = c.create_lifeboat(now)
+
+        self.assertDictEqual({'title': 'my cool arch', 'efi': os.path.join(
+            os.path.dirname(efi_path), f'lifeboat_{now}_linux.efi')}, lifeboat)
+        pass
+
+    def test_create_lifeboat_cleans_up_if_writing_fails(self):
+        efi_path = os.path.join(self.esp.name, 'EFI', 'Arch', 'linux.efi')
+        os.makedirs(os.path.dirname(efi_path), exist_ok=True)
+        with open(efi_path, 'w', encoding='utf8') as fp:
+            fp.write("my cool efi")
+        entry_path = self.create_entry('arch.conf', f'title my cool arch\nefi {efi_path}')
+        c = Config(entry_path)
+        c.load()
+        now = int(time.time())
+
+        # create the lifeboat file so that this will error out later
+        self.create_entry(f'lifeboat_{now}_arch.conf', 'already exists')
+        self.assertRaises(OSError, lambda: c.create_lifeboat(now))
+        # Make sure the lifeboat efi is cleaned up
+        self.assertFalse(os.path.exists(os.path.join(self.esp.name, 'EFI', 'Arch', f'lifeboat_{now}_linux.efi')))
+        pass
+
+    def create_loader(self, contents: str) -> str:
+        loader_name = os.path.join(self.esp.name, 'loader', 'loader.conf')
+        with open(loader_name, 'w', encoding='utf8') as fp:
+            fp.write(contents)
+        return loader_name
+
+    def create_entry(self, name: str, contents: str) -> str:
+        conf_name = os.path.join(self.esp.name, 'loader', 'entries', name)
+        with open(conf_name, 'w', encoding='utf8') as fp:
+            fp.write(contents)
+        return conf_name
 
 
 if __name__ == '__main__':
